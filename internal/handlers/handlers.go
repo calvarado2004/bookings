1ce8cfe2ab383fd70055fcfdbb02968104eec495
Carlos Alvarado <carlos-alvarado@outlook.com>
2022-06-22 00:48:20 -0400

    Calendar buttons

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -539,7 +539,47 @@ func (m *Repository) AdminAllNewReservations(w http.ResponseWriter, r *http.Requ
 
 //AdminCalendarReservations displays the reservation calendar
 func (m *Repository) AdminCalendarReservations(w http.ResponseWriter, r *http.Request) {
-	render.Template(w, r, "admin-calendar-reservations.page.html", &models.TemplateData{})
+	//assume that there is no month/year specified
+
+	now := time.Now()
+
+	if r.URL.Query().Get("y") != "" {
+
+		year, err := strconv.Atoi(r.URL.Query().Get("y"))
+		if err != nil {
+			helpers.ServerError(w, err)
+			return
+		}
+
+		month, err := strconv.Atoi(r.URL.Query().Get("m"))
+		if err != nil {
+			helpers.ServerError(w, err)
+			return
+		}
+
+		now = time.Date(year, time.Month(month), 1, 0, 0, 0, 0, time.UTC)
+
+	}
+
+	next := now.AddDate(0, 1, 0)
+	last := now.AddDate(0, -1, 0)
+	nextMonth := next.Format("01")
+	nextMonthYear := next.Format("2006")
+	lastMonth := last.Format("01")
+	lastMonthYear := last.Format("2006")
+
+	stringMap := make(map[string]string)
+
+	stringMap["next_month"] = nextMonth
+	stringMap["next_month_year"] = nextMonthYear
+	stringMap["last_month"] = lastMonth
+	stringMap["last_month_year"] = lastMonthYear
+	stringMap["this_month"] = now.Format("01")
+	stringMap["this_month_year"] = now.Format("2006")
+
+	render.Template(w, r, "admin-calendar-reservations.page.html", &models.TemplateData{
+		StringMap: stringMap,
+	})
 }
 
 //AdminShowReservations show one reservation
