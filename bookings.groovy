6c251930e6efd5abce74cb284aeec06d4259b6a5
Carlos Alvarado <carlos-alvarado@outlook.com>
2022-06-24 18:43:34 -0400

    Better way on the pipeline

--- bookings.groovy
+++ bookings.groovy
@@ -8,6 +8,7 @@ node('worker0') {
     
     env.HOME = "${WORKSPACE}"
     env.CONTAINER_IMAGE = 'docker.io/calvarado2004/bookings'
+    env.CONTAINER_IMAGE_INIT = 'docker.io/calvarado2004/bookings-init'
     env.GIT_PROJECT_CI = 'github.com/calvarado2004/bookings.git'
     env.GIT_PROJECT_CD = 'github.com/calvarado2004/bookings-cd.git'
 
@@ -23,8 +24,8 @@ node('worker0') {
     }
     
     stage ('Build Init Container') {
-           sh "sudo buildah bud -f Dockerfile-init -t ${CONTAINER_IMAGE}-init:${BUILD_TAG}"
-           sh "sudo buildah bud -f Dockerfile-init -t ${CONTAINER_IMAGE}-init:latest"
+           sh "sudo buildah bud -f Dockerfile-init -t ${CONTAINER_IMAGE_INIT}:${BUILD_TAG}"
+           sh "sudo buildah bud -f Dockerfile-init -t ${CONTAINER_IMAGE_INIT}:latest"
     }
 
     stage ('Push Container Images'){
@@ -33,15 +34,16 @@ node('worker0') {
             sh "sudo buildah login --username ${USERNAME} --password ${PASSWORD} docker.io"
             sh "sudo buildah push  ${CONTAINER_IMAGE}:${BUILD_TAG}"
             sh "sudo buildah push  ${CONTAINER_IMAGE}:latest"
-            sh "sudo buildah push  ${CONTAINER_IMAGE}-init:${BUILD_TAG}"
-            sh "sudo buildah push  ${CONTAINER_IMAGE}-init:latest"
+            sh "sudo buildah push  ${CONTAINER_IMAGE_INIT}:${BUILD_TAG}"
+            sh "sudo buildah push  ${CONTAINER_IMAGE_INIT}:latest"
         }
     }
     
     stage ('Modify Bookings image on Deployment') {
       withCredentials([string(credentialsId: 'git-token', variable: 'TOKEN')]) {
         git branch: 'main', url: "https://${GIT_PROJECT_CD}"
-        sh 'cat manifest/app-deployment.j2 | sed "s#{{ CONTAINER_IMAGE }}:{{ USED_TAG }}#${CONTAINER_IMAGE}:${BUILD_TAG}#g" > manifest/app-deployment.yaml'
+        sh 'cat manifest/app-deployment.j2 | sed "s#{{ CONTAINER_IMAGE }}:{{ USED_TAG }}#${CONTAINER_IMAGE}:${BUILD_TAG}#g" > manifest/app-deployment-temp.yaml'
+        sh 'cat manifest/app-deployment-temp.yaml | sed "s#{{ CONTAINER_IMAGE_INIT }}#${CONTAINER_IMAGE_INIT}#g" > manifest/app-deployment.yaml && rm -f manifest/app-deployment-temp.yaml'
         sh "git add . && git commit -m 'Change of image version of Golang App to ${BUILD_TAG}'"
         sh "git push https://${TOKEN}@${GIT_PROJECT_CD}"
       }
